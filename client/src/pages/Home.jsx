5801b982b4ffd56a9956a9d269c16f8292a24fe3
Chi Chi <christine.yennifer@gmail.com>
2026-04-16 19:29:31 +1000

    basic homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,2 +1,24 @@
-function Home() { return <h2>Welcome to MedProfile</h2>; }
+import { Container, Title, Text, Button, Group } from '@mantine/core';
+import { Link } from 'react-router-dom';
+
+function Home() { 
+    return (
+        <Container size={500} mt='xl' ta='center'>
+            <Title order={1} mb='md'>Welcome to MedProfile</Title>
+            <Text c='dimmed' mb='xl'>
+                Manage patient medication profiles securely.
+            </Text>
+            <Group justify='center' gap='md'>
+                <Button component={Link} to='/login'>
+                    Login
+                </Button>
+                <Button component={Link} to='/register' variant='outline'>
+                    Register
+                </Button>
+            </Group>
+        </Container>
+    );
+}
+
+
 export default Home;
