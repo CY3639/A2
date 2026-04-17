6bdf3fc2c1441d590afd42d750365e05deb4a732
Chi Chi <christine.yennifer@gmail.com>
2026-04-17 23:19:32 +1000

    added date css style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { MantineProvider } from '@mantine/core';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
  
 import Layout from './pages/Layout';
 import Home from './pages/Home';
