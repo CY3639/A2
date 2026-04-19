42434b9f7be8f184e02a39dd4ece1cb60420a6d9
Chi Chi <y3.yip@connect.qut.edu.au>
2026-04-19 09:20:00 +0000

    fix

--- server/src/middleware/authoriseRole.js
+++ server/src/middleware/authoriseRole.js
@@ -1,6 +1,6 @@
 const authoriseRole = (pharmacistOnly) => (req, res, next) => {
     if (pharmacistOnly && !req.user.isPharmacist) {
-        return res.statu(403).json({ error: "Not authorised. It can only be accessed by a Pharmacist" });
+        return res.status(403).json({ error: "Not authorised. It can only be accessed by a Pharmacist" });
     }
     next();
 };
